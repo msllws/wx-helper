a128b0c0832db4ed4332b9fed050a7a31584b353
liweishan <msllws@163.com>
2020-03-26 22:33:44 +0800

    demo

---
+++ src/WxHelper.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the overtrue/wechat.
+ *
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Lws;
+
+/**
+ * Class Factory.
+ *
+ * @method static \EasyWeChat\Payment\Application            payment(array $config)
+ * @method static \EasyWeChat\MiniProgram\Application        miniProgram(array $config)
+ * @method static \EasyWeChat\OpenPlatform\Application       openPlatform(array $config)
+ * @method static \EasyWeChat\OfficialAccount\Application    officialAccount(array $config)
+ * @method static \EasyWeChat\BasicService\Application       basicService(array $config)
+ * @method static \EasyWeChat\Work\Application               work(array $config)
+ */
+class WxHelper
+{
+    /**
+     * @param string $name
+     * @param array  $config
+     *
+     * @return \EasyWeChat\Kernel\ServiceContainer
+     */
+    public static function route($name, array $config)
+    {
+//        $namespace = Kernel\Support\Str::studly($name);
+        $application = "\\Lws\\{$namespace}\\Helper";
+
+        return new $application($config);
+    }
+
+    /**
+     * Dynamically pass methods to the application.
+     *
+     * @param string $name
+     * @param array  $arguments
+     *
+     * @return mixed
+     */
+    public static function __callStatic($name, $arguments)
+    {
+        return self::route($name, ...$arguments);
+    }
+}
--- src/Wxhelper/Library/Loader.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Wxhelper\Library;
-
-class Loader {
-    public function index(){
-        echo '送你一只老狗';
-    }
-}
